0375f856114437ff491457ead749030fb952b6b9
Kelvin Lee <kiyolee@gmail.com>
2020-10-29 13:31:50 +1100

    Update to expat-2.2.10.

--- win32/expat_version.h
+++ win32/expat_version.h
@@ -1,4 +1,4 @@
-#define EXPAT_VERSION_STR   "2.2.9"
+#define EXPAT_VERSION_STR   "2.2.10"
 #define EXPAT_VER_MAJOR     2
 #define EXPAT_VER_MINOR     2
-#define EXPAT_VER_REVISION  9
+#define EXPAT_VER_REVISION  10
