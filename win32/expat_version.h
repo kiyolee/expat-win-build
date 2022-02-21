3442d39763dd9bc704ae5b5dfcd2e0a813af5a59
Kelvin Lee <kiyolee@gmail.com>
2022-02-21 23:26:11 +1100

    Update to expat-2.4.5

--- win32/expat_version.h
+++ win32/expat_version.h
@@ -1,4 +1,4 @@
-#define EXPAT_VERSION_STR   "2.4.4"
+#define EXPAT_VERSION_STR   "2.4.5"
 #define EXPAT_VER_MAJOR     2
 #define EXPAT_VER_MINOR     4
-#define EXPAT_VER_REVISION  4
+#define EXPAT_VER_REVISION  5
