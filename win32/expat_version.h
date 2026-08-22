c4e3601fdaffa1b15f3895834bccbaebc2bfb4ce
Kelvin Lee <kiyolee@gmail.com>
2026-08-23 07:35:54 +1000

    Update to 2.8.3

--- win32/expat_version.h
+++ win32/expat_version.h
@@ -1,4 +1,4 @@
-#define EXPAT_VERSION_STR   "2.8.2"
+#define EXPAT_VERSION_STR   "2.8.3"
 #define EXPAT_VER_MAJOR     2
 #define EXPAT_VER_MINOR     8
-#define EXPAT_VER_REVISION  2
+#define EXPAT_VER_REVISION  3
